6965880bee129b75bd53bbb93c4ef4f34e587268
Simon Wörpel <simon.woerpel@pm.me>
2025-12-21 22:48:58 +0100

    🚧 (model) Public exports

--- ftm_lakehouse/model/__init__.py
+++ ftm_lakehouse/model/__init__.py
@@ -1,31 +1,26 @@
 """Data models for ftm_lakehouse."""
 
-from ftm_lakehouse.model.catalog import CatalogModel, DatasetModel
+from ftm_lakehouse.model.crud import Crud, CrudAction, CrudResource
+from ftm_lakehouse.model.dataset import CatalogModel, DatasetModel
 from ftm_lakehouse.model.file import File, Files
-from ftm_lakehouse.model.job import DEFAULT_USER, DatasetJobModel, JobModel
-from ftm_lakehouse.model.mapping import (
-    DatasetMapping,
-    EntityMapping,
-    Mapping,
-    PropertyMapping,
-    mapping_origin,
-)
+from ftm_lakehouse.model.job import DatasetJobModel, JobModel
+from ftm_lakehouse.model.mapping import DatasetMapping, mapping_origin
 
 __all__ = [
     # Catalog
     "CatalogModel",
     "DatasetModel",
+    # Crud
+    "Crud",
+    "CrudAction",
+    "CrudResource",
     # File
     "File",
     "Files",
     # Job
-    "DEFAULT_USER",
     "DatasetJobModel",
     "JobModel",
     # Mapping
     "DatasetMapping",
-    "EntityMapping",
-    "Mapping",
-    "PropertyMapping",
     "mapping_origin",
 ]
