6ed90fcf8b6ba5eb669bd18848e6c09f73619e83
Simon Wörpel <simon.woerpel@pm.me>
2026-08-24 17:31:18 +0200

    🔖 Bump version: 0.4.0 → 0.5.0

--- ftm_lakehouse/__init__.py
+++ ftm_lakehouse/__init__.py
@@ -14,7 +14,7 @@ from ftm_lakehouse.lake import (
 )
 from ftm_lakehouse.model.dataset import DatasetModel, set_model_class
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 __all__ = [
     "Catalog",
--- ftm_lakehouse/core/settings.py
+++ ftm_lakehouse/core/settings.py
@@ -7,7 +7,7 @@ from pydantic_settings import SettingsConfigDict
 
 CHECKSUM_ALGORITHM = "sha256"  # never change this! ;)
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 SECRETS_DIR = Path("/run/secrets")
 """Docker secrets mount"""
